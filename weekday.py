05ff434d8e82fc3bef07b52d46ded86df0be499b
Samarth <samarthgoradia@gmail.com>
2026-03-16 16:17:00 +0530

    conditionals practice 4

---
+++ weekday.py
@@ -0,0 +1,7 @@
+day = str(input("Enter day: "))
+day = day.lower().strip()
+
+if day == "monday" or day == "tuesday" or day == "wednesday" or day == "thursday" or day == "friday":
+    print("Weekday")
+else:
+    print("Weekend")
